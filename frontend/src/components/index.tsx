b5fdf10186d96844d7aae6bdee677de9b537357e
imukyee <imukyee@gmail.com>
2022-09-14 15:07:26 +0900

    feat: Input컴포넌트 제작 & fontSize, fontWeight 추가

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,11 +1,1 @@
-import React from 'react';
-
-const index = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
-
-export default index;
+export { default as Input } from './Input';
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Button from "../components/Button";
+import { Input } from "../components"
 
 const MainPage = () => {
   return (
     <div>
+      하이
       <Button></Button>
+      <Input></Input>
+      <input color="red"/>
     </div>
   );
 };
