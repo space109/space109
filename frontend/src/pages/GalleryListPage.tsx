9260ef3f73ceba7d5f3950fafc9f70a53a772c82
황상윤 <s_infinite@naver.com>
2022-09-15 11:28:57 +0900

    Fix: 버튼 컴포넌트 수정사항 수정

--- frontend/src/pages/GalleryListPage.tsx
+++ frontend/src/pages/GalleryListPage.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import GalleryCard from "../components/Card/GalleryCard";
 
-
 // type Props = {};
 
 function GalleryListPage() {
   return (
     <div>
-      <GalleryCard/>
+      <GalleryCard theme="SSAFY" title="제목: 여름엔 해변으로!" />
     </div>
   );
 }
