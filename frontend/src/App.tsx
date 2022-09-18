a014b1549949ef5a219f691105620a5cb11c5347
imukyee <imukyee@gmail.com>
2022-09-19 05:15:41 +0900

    Feat: MainUI Section별로 Scroll 넘기기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <NavBar />
+        {/* <NavBar /> */}
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/monthlyTheme" element={<MonthlyThemePage />}></Route>
