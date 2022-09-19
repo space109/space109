9d34a05dbecd077178bcc3736c15a3b4acb38451
s_infinite <s_infinite@naver.com>
2022-09-19 09:22:05 +0900

    Modify: 코드 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,12 +15,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />}/>
-          <Route path="/monthlyTheme" element={<MonthlyThemePage />}/>
-          <Route path="/gallery" element={<GalleryListPage />}/>
-          <Route path="/signUp" element={<SignUpPage />}/>
-          <Route path="/profile" element={<ProfilePage />}/>
-          <Route path="/*" element={<NotFoundPage />}/>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/monthlyTheme" element={<MonthlyThemePage />} />
+          <Route path="/gallery" element={<GalleryListPage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </div>
