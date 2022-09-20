fde5fd97be7461af3b563e24629cdab6156e662a
s_infinite <s_infinite@naver.com>
2022-09-20 10:54:52 +0900

    Modify: 네브바 수정 & 카드 컴포넌트 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,11 +15,13 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/monthlyTheme" element={<MonthlyThemePage />} />
-          <Route path="/gallery" element={<GalleryListPage />} />
-          <Route path="/signUp" element={<SignUpPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route element={<NavBar />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/monthlyTheme" element={<MonthlyThemePage />} />
+            <Route path="/gallery" element={<GalleryListPage />} />
+            <Route path="/signUp" element={<SignUpPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/Card/GalleryCard.tsx
+++ frontend/src/components/Card/GalleryCard.tsx
@@ -16,9 +16,17 @@ const Card = styled.div<PropStyle>`
   /* max-height: 625px; */
   width: ${(props) => props.width};
   height: 625px;
-  background-color: var(--spinach-200);
+  background: url("https://cdn.imweb.me/upload/S201906191c3595f104fd6/4dacf6c8274de.jpg");
   color: var(--grey-100);
   position: relative;
+  transition: 1s ease-out;
+
+  &:hover {
+    background: rgb(0, 0, 0, 0);
+    & div .cardContent {
+      opacity: 1;
+    }
+  }
 `;
 
 const CardMain = styled.div<PropStyle>`
@@ -27,6 +35,9 @@ const CardMain = styled.div<PropStyle>`
   top: 20px;
   left: 20px;
   position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const CardTheme = styled.div`
@@ -61,40 +72,54 @@ const CardTitle = styled.div`
 const CardContent = styled.div`
   /* position: absolute; */
   border: 0;
-  font-size: 100%;
-  font-style: inherit;
-  font-weight: inherit;
   outline: 0;
   /* vertical-align: baseline; */
   padding: 0;
-  margin: 0 0 5px 0;
-  word-break: keep-all;
+  margin: 20px 0 5px 0;
+  word-break: all;
 
   font-family: "Pretendard Variable";
   font-style: normal;
   font-weight: 600;
   font-size: 18px;
   line-height: 140%;
+  opacity: 0;
+  /* transition: 0.5s; */
   /* or 25px */
 
   display: flex;
   align-items: center;
 `;
 
+const BgImage = styled.div<PropStyle>`
+  position: absolute;
+  width: ${(props) => props.width};
+  height: 625px;
+  background-color: var(--spinach-200);
+  z-index: -1;
+  color: var(--grey-100);
+`;
+
 const CardAuthor = styled.div``;
 
 function GalleryCard({ theme, title, width = "425px" }: Props) {
   return (
-    <Card width={width}>
-      <CardMain width={width}>
-        <CardTheme>테마: {theme ? theme : "테마"}</CardTheme>
-        <CardTitle>{title ? title : "제목"}</CardTitle>
-        <CardContent>
-          여름 휴가를 가고 싶지만 시간 이슈로 가지 못한 저를 위로하고자 그린
-          해변 그림 컬렉션입니다. 저의 상상과 꿈을 담아 그렸습니다.
-        </CardContent>
-      </CardMain>
-    </Card>
+    <div>
+      <BgImage width={width} />
+      <Card width={width}>
+        <CardMain width={width}>
+          <div>
+            <CardTheme>테마: {theme ? theme : "테마"}</CardTheme>
+            <CardTitle>{title ? title : "제목"}</CardTitle>
+            <CardContent className="cardContent">
+              여름 휴가를 가고 싶지만 시간 이슈로 가지 못한 저를 위로하고자 그린
+              해변 그림 컬렉션입니다. 저의 상상과 꿈을 담아 그렸습니다.
+            </CardContent>
+          </div>
+          <CardAuthor>둘기</CardAuthor>
+        </CardMain>
+      </Card>
+    </div>
   );
 }
 export default GalleryCard;
