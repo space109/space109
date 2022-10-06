43baf707b7c743ed379e18884ba42021ef0e6067
황상윤 <s_infinite@naver.com>
2022-10-06 10:45:39 +0900

    Fix: 페이지 좌우 마진 수정

--- frontend/src/pages/GalleryListPage.js
+++ frontend/src/pages/GalleryListPage.js
@@ -11,8 +11,22 @@ const SearchArea = styled.div`
   align-items: center;
   background: var(--grey-600);
   box-shadow: -1px 3px 5px rgba(0, 0, 0, 0.5);
+`;
 
-`
+const Container = styled(Div)`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 1800px;
+
+  margin: 0 auto;
+
+  @media (max-width: 1920px) {
+    width: calc(100vw - 120px);
+  }
+  @media (max-width: 1366px) {
+    width: calc(100vw - 64px);
+  }
+`;
 
 const Background = styled.div`
   position: fixed;
@@ -22,34 +36,30 @@ const Background = styled.div`
   min-height: 100vh;
   background-color: var(--grey-150);
   z-index: -10;
-`
+`;
 
 const FilterDiv = styled.div`
   box-sizing: border-box;
   width: 100%;
   margin: 40px 0 50px 0;
-  padding: 0 60px 0 60px;
-  @media screen and (max-width: ${screenSizes.xl + "px"}) {
-    padding: 0% 32px 0% 32px;
-  }
-`
+`;
 
 const Content = styled.div`
   box-sizing: border-box;
-`
+`;
 
 const SearchDiv = styled.div`
+  margin: 0 auto;
   width: 60%;
   @media screen and (max-width: ${screenSizes.md + "px"}) {
     width: 80%;
   }
-`
+`;
 
 function GalleryListPage({ load }) {
-
-  const [ gallerys, setGallerys ] = useState();
-  const [ searchValue, setSearchValue ] = useState();
-  const [ theme, setTheme ] = useState(0);
+  const [gallerys, setGallerys] = useState();
+  const [searchValue, setSearchValue] = useState();
+  const [theme, setTheme] = useState(0);
 
   // 필터링과 검색어에 따라 보여줄 갤러리 정보 깎는 함수
   // if문 너무 더럽다ㅠ 나중에 수정
@@ -61,56 +71,65 @@ function GalleryListPage({ load }) {
       if (searchValue) {
         let result = [];
         for (let i = 0; i < datas.length; i++) {
-          if (datas[i].description.includes(searchValue) || datas[i].title.includes(searchValue)) {
+          if (
+            datas[i].description.includes(searchValue) ||
+            datas[i].title.includes(searchValue)
+          ) {
             result.push(datas[i]);
           }
         }
         setGallerys(result);
-      } 
+      }
       // 검색어가 없을 경우
       else {
         setGallerys(datas);
       }
     }
-    // 전체 선택 인 경우 
+    // 전체 선택 인 경우
     else {
       const datas = await getGalleryList();
       if (searchValue) {
         let result = [];
         for (let i = 0; i < datas.length; i++) {
-          if (datas[i].description.includes(searchValue) || datas[i].title.includes(searchValue)) {
+          if (
+            datas[i].description.includes(searchValue) ||
+            datas[i].title.includes(searchValue)
+          ) {
             result.push(datas[i]);
           }
         }
         setGallerys(result);
-      }
-      else {
+      } else {
         setGallerys(datas);
-      } 
+      }
     }
-  }
+  };
 
   useEffect(() => {
     getGallerys(theme, searchValue);
-  }, [theme, searchValue])
+  }, [theme, searchValue]);
 
   return (
     <>
-      <Background>
-      </Background>
+      <Background></Background>
       <NavArea />
+
       <SearchArea>
-        <SearchDiv>
-          <SearchBar setValue={setSearchValue}/>
-        </SearchDiv>
-        <FilterDiv>
-          <FilterButtons List={CATEGORY} setValue={setTheme} />
-        </FilterDiv>
+        <Container>
+          <SearchDiv>
+            <SearchBar setValue={setSearchValue} />
+          </SearchDiv>
+          <FilterDiv>
+            <FilterButtons List={CATEGORY} setValue={setTheme} />
+          </FilterDiv>
+        </Container>
       </SearchArea>
-      <Content>
-        <GalleryList gallerys={gallerys} load={load}></GalleryList>
-        {/* <GalleryList></GalleryList> */}
-      </Content>
+      <Container>
+        <Content>
+          <GalleryList gallerys={gallerys} load={load}></GalleryList>
+          {/* <GalleryList></GalleryList> */}
+        </Content>
+      </Container>
     </>
   );
 }
